b4f8dea9e3db4cf070a303a52882cff8d20b87d3
CorgiMuzi <kjhcorgi99@gmail.com>
2023-07-27 15:11:19 +0900

    Revert "Abstrac Factory Method pattern added"
    This reverts commit 4cc5fd99471cd98d84686c7f1ec717d38e1ee14b.

--- CPPDesignPattern/CPPDesignPattern/AbstractFactoryMethod.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Player.h"
-#include "Armor.h"
-#include "Forge.h"
-
-using namespace AbstractFactoryMethod;
--- CPPDesignPattern/CPPDesignPattern/FactoryMethod.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Player.h"
-#include "Monster.h"
-#include "MonsterSpawner.h"
-
-using namespace FactoryMethod;
